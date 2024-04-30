8706f74ec7dc882d20564435b42a238a3f97eecd
flaamer <88228748+FLAAMER@users.noreply.github.com>
2024-04-30 17:14:53 +0530

    Change div to black when entered by mouse pointer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 for (let i = 0; i < 16*16; i++) {
     const container = document.querySelector("#container")
     const div = document.createElement("div")
+    div.addEventListener("mouseenter", () => div.style.backgroundColor = "black")
     container.appendChild(div)
 }
