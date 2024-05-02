980abce19bc9813f83ce07715355199807d5d1f3
flaamer <88228748+FLAAMER@users.noreply.github.com>
2024-05-02 12:53:52 +0530

    Add a button to resize the grid
     - Add a button on the top of the screen that will send the user a popup
       asking for the number of squares per side for the new grid. Once
       entered, the existing grid will be removed, and a new grid qill be
       generated in the same total space as before

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-for (let i = 0; i < 16*16; i++) {
-    const container = document.querySelector("#container")
-    const div = document.createElement("div")
-    div.addEventListener("mouseenter", () => div.style.backgroundColor = "black")
-    container.appendChild(div)
+let numberOfSquares = 16
+
+const resizeButton = document.querySelector("button#resize")
+resizeButton.addEventListener("click", () => gridResize())
+
+function generateGrid(numberOfSquares) {
+    if (numberOfSquares <=100) {
+        for (let i = 0; i < numberOfSquares*numberOfSquares; i++) {
+            const container = document.querySelector("#container")
+            const div = document.createElement("div")
+            div.className = "grid"
+            div.addEventListener("mouseenter", () => div.style.backgroundColor = "indianred")
+            const padding = 32/(numberOfSquares*2)
+            div.style.padding =`${padding}vw`
+            container.appendChild(div)
+        }
+    } else {
+        alert("Invalid")
+    }
+}
+
+generateGrid(numberOfSquares)
+
+function gridResize() {
+    console.log(numberOfSquares)
+    for (let i = 0; i < numberOfSquares*numberOfSquares; i++) {
+    const container = document.querySelector("div#container")
+    const div = document.querySelector("div.grid")
+    container.removeChild(div)
+    }
+    numberOfSquares = prompt("How many squares per side of the grid (max 100)")
+    generateGrid(numberOfSquares)
 }
